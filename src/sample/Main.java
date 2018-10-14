3a9d2ab86c00091d32e6b8f55a760b299bb8049c
Brenno Serrato <brennoserrato@alunos.utfpr.edu.br>
2018-10-14 17:04:54 -0300

    organização

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Cliente.fxml"));
         primaryStage.setTitle("RMI CLIENT World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
