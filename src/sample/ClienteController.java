83770d98f8c4c418450ae4031326de031f357b96
GustavoDeCamargo <guustavocamargo@gmail.com>
2018-10-14 23:17:41 -0300

    Adicionada ClienteController e Layout Basico Cliente

---
+++ src/sample/ClienteController.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class ClienteController {
+    public void consultarPassagem(){
+
+    }
+    public void consultarHospedagem(){
+
+    }
+    public void consultarPacote(){
+
+    }
+    public void demonstrarInteresse(){
+
+    }
+}
